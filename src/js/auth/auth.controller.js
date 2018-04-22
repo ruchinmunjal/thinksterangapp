388ede74c7849d8df7c38d6d58bd5381df12addb
Ruchin Munjal <emailruchin@gmail.com>
2018-04-22 22:19:27 +0100

    service implemented in controller

--- src/js/auth/auth.controller.js
+++ src/js/auth/auth.controller.js
@@ -1,13 +1,28 @@
 class AuthCtrl {
-    constructor($state){
+    constructor(User,$state){
         'ngInject';
+        
+        this._User=User;
 
         this.title=$state.current.title;
         this.authType=$state.current.name.replace('app.','');
     }
     submitForm(){
         this.isSubmitting=true;
-        console.log(this.formData);
+        //console.log(this.formData);
+        this._User.attemptAuth(this.authType,this.formData)
+                .then(
+                    (res)=>{
+                        this.isSubmitting=false;
+                        console.log(res);
+                    },
+                    (err)=>{
+                        //debugger;
+                        console.log(err);
+                        this.isSubmitting=false;
+                        console.log(err.data.errors);
+                    }
+                );
     }
 }
 
--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,23 +1,24 @@
-export default class User{
-    constructor(AppConstants,$http){
+export default class User {
+    constructor(AppConstants, $http) {
         'ngInject';
-        this._AppConstants=AppConstants;
-        this._$http=$http;
-        this.current=null;
+        this._AppConstants = AppConstants;
+        this._$http = $http;
+
+        this.current = null;
     }
 
-    attemptAuth(type,credentials){
-        let route= (type==='login') ? '/login':'';
+    attemptAuth(type, credentials) {
+        let route = (type === 'login') ? '/login' : '';
         return this._$http({
-            url:this._AppConstants.api+'/users'+route,
-            method:'POST',
-            data:{
-                user:credentials
+            url: this._AppConstants.api + '/users' + route,
+            method: 'POST',
+            data: {
+                user: credentials
             }
         }).then(
             (res) => {
-                this.current=res.data.user;
-                return user;
+                this.current = res.data.user;
+                return res;
             }
         );
     }
