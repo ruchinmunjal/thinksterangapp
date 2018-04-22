052a3b3546333a121e5e131c17532a0666706b9a
Ruchin Munjal <emailruchin@gmail.com>
2018-04-22 22:27:55 +0100

    service implemetation completed

--- src/js/auth/auth.controller.js
+++ src/js/auth/auth.controller.js
@@ -1,28 +1,29 @@
 class AuthCtrl {
-    constructor(User,$state){
+    constructor(User, $state) {
         'ngInject';
-        
-        this._User=User;
 
-        this.title=$state.current.title;
-        this.authType=$state.current.name.replace('app.','');
+        this._User = User;
+
+        this.title = $state.current.title;
+        this.authType = $state.current.name.replace('app.', '');
     }
-    submitForm(){
-        this.isSubmitting=true;
+    submitForm() {
+        this.isSubmitting = true;
         //console.log(this.formData);
-        this._User.attemptAuth(this.authType,this.formData)
-                .then(
-                    (res)=>{
-                        this.isSubmitting=false;
-                        console.log(res);
-                    },
-                    (err)=>{
-                        //debugger;
-                        console.log(err);
-                        this.isSubmitting=false;
-                        console.log(err.data.errors);
-                    }
-                );
+        this._User.attemptAuth(this.authType, this.formData)
+            .then(
+                (res) => {
+                    this.isSubmitting = false;
+                    console.log(res);
+                },
+                (err) => {
+                    //debugger;
+                    //console.log(err);
+                    this.isSubmitting = false;
+                    console.log(err.data.errors);
+                    this.errors = err.data.errors;
+                }
+            );
     }
 }
 
